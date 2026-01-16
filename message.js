28f2c238d01af59461d72216c911ec2a31435f9d
mzcheong97 <mark@intermatch.co>
2026-01-16 07:34:18 -0800

    add generatemessage function

--- message.js
+++ message.js
@@ -1,3 +1,11 @@
 const openings = ["Hello", "Hi", "Greetings", "Hey"];
 const prompts = ["How are you?", "What's up?", "How's it going?", "What are you doing?"];
 const twists = ["Just kidding!", "Surprise!", "Guess what?", "You won't believe this!"];
+
+function generateMessage() {
+    const opening = openings[Math.floor(Math.random() * openings.length)];
+    const prompt = prompts[Math.floor(Math.random() * prompts.length)];
+    const twist = twists[Math.floor(Math.random() * twists.length)];
+    return `${opening} ${prompt} ${twist}`;
+}
+console.log(generateMessage());

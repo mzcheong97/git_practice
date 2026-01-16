fa5fee0147d416487ad200a8faa3e67b0a76fe29
mzcheong97 <mark@intermatch.co>
2026-01-16 07:22:47 -0800

    establish arrays

---
+++ message.js
@@ -0,0 +1,3 @@
+const openings = ["Hello", "Hi", "Greetings", "Hey"];
+const prompts = ["How are you?", "What's up?", "How's it going?", "What are you doing?"];
+const twists = ["Just kidding!", "Surprise!", "Guess what?", "You won't believe this!"];
